75a572c039df1f0617394cbaeabd979d3b06c9b7
tonuraydemir <144567590+tonuraydemir@users.noreply.github.com>
2024-12-03 19:48:19 +0100

    C#CurrencyConverter
    A simple currency converter application built in C#. This project allows users to convert between different currencies

---
+++ projectCurrencyConventer/c#CurrencyConverter/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+      
+        var exchangeRates = new Dictionary<string, double>
+        {
+            { "USD", 1.0 }, { "EUR", 0.97 }, { "TRY", 36.0 }, { "GBP", 0.56 }
+            ,{"KM",1.93}
+        };
+
+   Console.WriteLine("Welcome to the Currency Converter Application ");
+  Console.WriteLine("Available Currencies: KM , TRY , USD , EUR, GBP");
+
+  Console.Write("Enter the source currency (e.g., KM): ");
+   string sourceCurrency = Console.ReadLine()?.ToUpper();
+   Console.Write("Enter the target currency (e.g., TRY): ");
+      string targetCurrency = Console.ReadLine()?.ToUpper();
+       Console.Write("Amount: ");
+
+        if (double.TryParse(Console.ReadLine(), out double amount) &&
+          exchangeRates.TryGetValue(sourceCurrency, out double sourceRate) &&
+            exchangeRates.TryGetValue(targetCurrency, out double targetRate))
+        {
+            double result = amount * (targetRate / sourceRate);
+            Console.WriteLine($"{amount} {sourceCurrency} = {result} {targetCurrency}");
+            Console.WriteLine("Logged Out...");
+            Console.ReadLine();
+        }
+        else
+        {
+            Console.WriteLine("Invalid Values. Please check currency codes and amount.");
+            Console.ReadLine();
+        }
+        Console.WriteLine("Logged Out...");
+    }
+
+}
+
